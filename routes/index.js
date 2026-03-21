58268c62e09f984b7d2d9f544361c312829267ac
glenn-d-soriano <glenndannielsoriano@gmail.com>
2026-03-21 17:38:05 -0600

    fix route/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,32 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 const validation = require('../middleware/validate');
 
-// Home Route (This fixes the 404 error!) 
+// Home Route
 routes.get('/', (req, res) => {
   res.send('Travel Destinations API is Running! Go to /api-docs for documentation.');
 });
 
-// Destinations Routes 
+// Destinations Routes (GET and DELETE)
 routes.get('/destinations', destController.getAll);
-routes.post('/destinations', destController.createDestination);
-routes.put('/destinations/:id', destController.updateDestination);
 routes.delete('/destinations/:id', destController.deleteDestination);
+
+// Validation Rules applied to POST and PUT 
+routes.post(
+  '/destinations', 
+  validation.destinationValidationRules(), 
+  validation.validate, 
+  destController.createDestination
+);
+
+routes.put(
+  '/destinations/:id', 
+  validation.destinationValidationRules(), 
+  validation.validate, 
+  destController.updateDestination
+);
 
 // Swagger Documentation 
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-
-
-// Validation rules and the validate checker to POST and PUT
-routes.post('/destinations', validation.destinationValidationRules(), validation.validate, destController.createDestination);
-routes.put('/destinations/:id', validation.destinationValidationRules(), validation.validate, destController.updateDestination);
 
 module.exports = routes;
