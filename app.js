63531b28b30d9d4c21099ebba77ae508128216df
glenn-d-soriano <glenndannielsoriano@gmail.com>
2026-03-26 00:05:35 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const passport = require('passport'); // Added Passport
+const session = require('express-session'); // Added Session
+const GitHubStrategy = require('passport-github2').Strategy; // Added Strategy
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret", // This can be any string, but it's required
+    resave: false,
+    saveUninitialized: true,
+  }))
+  // Initialize Passport and Session middleware
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(cors({ methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}))
+  .use(cors({ origin: '*' }))
   .use('/', require('./routes'));
+
+// Configure the GitHub Strategy
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // Return for profile
+    return done(null, profile);
+  }
+));
+
+// Tell Passport how to "remember" the user
+passport.serializeUser((user, done) => { done(null, user); });
+passport.deserializeUser((user, done) => { done(null, user); });
 
 mongodb.initDb((err) => {
   if (err) {
